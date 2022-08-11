5773f886a4fd84a2f537664b038b2f994d0c4818
Neeejm <nejmzakaria@gmail.com>
2022-08-11 20:03:49 +0100

    refactoring test

--- src/test/java/com/neeejm/inventory/repositories/OrderRepositoryTest.java
+++ src/test/java/com/neeejm/inventory/repositories/OrderRepositoryTest.java
@@ -2,12 +2,9 @@ package com.neeejm.inventory.repositories;
 
 import com.neeejm.inventory.models.Order;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -21,22 +18,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class OrderRepositoryTest {
 
+    private final OrderRepository underTest;
+
     @Autowired
-    private OrderRepository underTest;
-
-    private Order order;
-
-    @BeforeEach
-    void setup() throws ParseException {
-        order = Order.builder()
-                .createdAt(
-                        new SimpleDateFormat("yyyy-MM-dd")
-                                .parse("2022-01-01")
-                ).reference("or-01")
-                .expectedShipmentDate(new Date())
-                .actualShipmentDate(new Date())
-                .status(Order.Status.ORDERED)
-                .build();
+    OrderRepositoryTest(OrderRepository underTest) {
+        this.underTest = underTest;
     }
 
     @AfterEach
@@ -50,15 +36,38 @@ class OrderRepositoryTest {
             names = {"SALE", "PURCHASE"}
     )
     @DisplayName("Check if orders are found by type and ordered by creation date descending")
-    void checkFindByTypeOrderByCreatedAtDesc(Order.Type type) {
+    void checkFindByTypeOrderByCreatedAtDesc(Order.Type type) throws ParseException {
         // Given
-        order.setType(type);
-        underTest.save(order);
+        Order orderOne = Order.builder()
+                .createdAt(
+                        new SimpleDateFormat("yyyy-MM-dd")
+                                .parse("2022-01-01")
+                ).reference("or-01")
+                .expectedShipmentDate(new Date())
+                .actualShipmentDate(new Date())
+                .status(Order.Status.ORDERED)
+                .type(type)
+                .build();
+        Order orderTwo = Order.builder()
+                .createdAt(
+                        new SimpleDateFormat("yyyy-MM-dd")
+                                .parse("2021-01-01")
+                ).reference("or-02")
+                .expectedShipmentDate(new Date())
+                .actualShipmentDate(new Date())
+                .status(Order.Status.ORDERED)
+                .type(type)
+                .build();
+        underTest.save(orderOne);
+        underTest.save(orderTwo);
 
         // When
         List<Order> expected = underTest.findByTypeOrderByCreatedAtDesc(type);
 
         // Then
-        assertThat(expected).containsExactly(order);
+        assertThat(expected)
+                .isNotEmpty()
+                .hasSize(2)
+                .containsExactly(orderOne, orderTwo);
     }
 }
