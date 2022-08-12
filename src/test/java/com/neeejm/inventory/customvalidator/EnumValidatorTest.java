f8398818bc8fa1d59782f9f76ad2b9c3fa3cce51
Neeejm <nejmzakaria@gmail.com>
2022-08-12 07:33:48 +0100

    adding custom validators test

---
+++ src/test/java/com/neeejm/inventory/customvalidator/EnumValidatorTest.java
@@ -0,0 +1,66 @@
+package com.neeejm.inventory.customvalidator;
+
+import com.neeejm.inventory.models.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.Payload;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class EnumValidatorTest {
+    private static final EnumValidator enumValidator = new EnumValidator();
+    private static ValidEnum validEnum;
+
+    @BeforeAll
+    static void setEnumValidator() {
+        validEnum = new ValidEnum() {
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return null;
+            }
+
+            @Override
+            public Class<? extends Enum<?>> enumClass() {
+                return Order.Status.class;
+            }
+
+            @Override
+            public String message() {
+                return "Value not valid";
+            }
+
+            @Override
+            public Class<?>[] groups() {
+                return new Class[0];
+            }
+
+            @Override
+            public Class<? extends Payload>[] payload() {
+                return new Class[0];
+            }
+        };
+        enumValidator.initialize(validEnum);
+    }
+
+    @Test
+    void validEnumValue() {
+        // Given
+        String enumValue = "ORDERED";
+
+        // When
+        boolean expected = enumValidator.isValid(enumValue, null);
+
+        // Then
+        assertThat(expected).isTrue();
+    }
+}
