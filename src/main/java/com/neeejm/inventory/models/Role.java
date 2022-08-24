ccc6621c41c8c21589adfc81624481ca8051dc60
Neeejm <nejmzakaria@gmail.com>
2022-08-24 02:55:11 +0100

    fixing gitignore bin folder

--- src/main/java/com/neeejm/inventory/models/Role.java
+++ src/main/java/com/neeejm/inventory/models/Role.java
@@ -16,7 +16,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.Hibernate;
 
